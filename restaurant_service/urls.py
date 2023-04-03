ea97f4654e52708c8a654590487939897dd7f407
Maksym_Sylantiev <maxym2s@yahoo.com>
2023-04-03 14:22:10 +0300

    Created login and logout templates

--- restaurant_service/urls.py
+++ restaurant_service/urls.py
@@ -24,4 +24,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("restaurant.urls", namespace="kitchen")),
-]
+    path("accounts/", include("django.contrib.auth.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
